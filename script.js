e25f54b5b631310b0409843dfb6750e558dea6c4
__tanglymeng <tanglymeng2@gmail.com>
2024-07-30 19:17:12 +0700

    seo test

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+
+// Example of a simple function to demonstrate interactivity
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("JavaScript loaded and ready!");
+    // Add any JavaScript interactivity here
+});
